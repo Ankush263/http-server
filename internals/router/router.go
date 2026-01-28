953690ee4dec7168e477421b7da13d3b55275cb0
Ankush263 <ankushbanik263@gmail.com>
2026-01-29 01:28:50 +0530

    update routers

--- internals/router/router.go
+++ internals/router/router.go
@@ -2,14 +2,51 @@ package router
 
 import (
 	"net/http"
-
-	"github.com/Ankush263/http-server/internals/handler"
 )
 
-func New() *http.ServeMux {
-	mux := http.NewServeMux()
+// Define the handler function signature.
+type HandlerFunc func(http.ResponseWriter, *http.Request)
 
-	mux.HandleFunc("/health", handler.Health)
+// ---------------------------------
+// routes := {
+// 	"/health": {
+// 		"GET": HealthHandler,
+// 	},
+// 	"/users": {
+// 		"GET":  GetUsers,
+// 		"POST": CreateUser,
+// 	},
+// }
+// ---------------------------------
+type Router struct {
+	// map[path]map[method]handler
+	routes map[string]map[string]HandlerFunc
+}
 
-	return mux
+// This will initialize the router map and return a new ready to use router
+func New() *Router {
+	return &Router{
+		routes: make(map[string]map[string]HandlerFunc),
+	}
+}
+
+// This function is used for creating new routes.
+// Usage: router.Handle("GET", "/health", Health)
+func (r *Router) Handle(method, path string, handler HandlerFunc) {
+	if r.routes[path] == nil {
+		r.routes[path] = make(map[string]HandlerFunc)
+	}
+	r.routes[path][method] = handler
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if methods, ok := r.routes[req.URL.Path]; ok {
+		if handler, ok := methods[req.Method]; ok {
+			handler(w, req)
+			return
+		}
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.Error(w, "Not Found", http.StatusNotFound)
 }
