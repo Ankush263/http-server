f465ffd97e4d207d66ae82ad4fa53fb18d643692
Ankush263 <ankushbanik263@gmail.com>
2026-01-31 18:09:15 +0530

    Added server and update app

--- internals/app/app.go
+++ internals/app/app.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/Ankush263/http-server/internals/config"
+	"github.com/Ankush263/http-server/internals/handler"
+	"github.com/Ankush263/http-server/internals/middleware"
 	"github.com/Ankush263/http-server/internals/router"
 	"github.com/Ankush263/http-server/internals/server"
 )
@@ -12,9 +14,14 @@ func Run() {
 	cfg := config.Load()
 
 	r := router.New()
+	r.Handle("GET", "/health", handler.Health)
+	r.Handle("POST", "/echo", handler.Echo)
+	
+	loggedRouter := middleware.Logger(r)
 
-	srv := server.New(cfg.Port, r)
+	srv := server.New(cfg.Port, loggedRouter)
 
-	log.Printf("Server running on port %s\n: ", cfg.Port)
-	log.Fatal(srv.Start())
+	if err := srv.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
--- internals/server/server.go
+++ internals/server/server.go
@@ -1,19 +1,43 @@
 package server
 
-import "net/http"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
 type Server struct {
-	port string
-	router http.Handler
+	httpServer *http.Server
 }
 
-func New(port string, router http.Handler) *Server {
+func New(port string, handler http.Handler) *Server{
 	return &Server{
-		port: port,
-		router: router,
+		httpServer: &http.Server{
+			Addr: ":" + port,
+			Handler: handler,
+		},
 	}
 }
 
 func (s *Server) Start() error {
-	return http.ListenAndServe(":" + s.port, s.router)
+	go func() {
+		log.Println("Server Started")
+		if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<- quit
+
+	log.Printf("Shutting down the server")
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	return s.httpServer.Shutdown(ctx)
 }
